0d4572b74f26c8f8d55a81a0bbd3489ae21ea5e5
hammrxu <hammrxu@gmail.com>
2022-11-07 13:00:55 -0500

    new updates

--- src/components/Update.js
+++ src/components/Update.js
@@ -7,7 +7,7 @@ function Update() {
             <div className='alert-container my-container'>
                 <div role="alert" className="fade alert alert-primary show">
                     <div className="alert-heading h4">Update</div>
-                    <p>Currently, I am Open and searching for a React Developer job opportunity. Check  out at&nbsp;<a className="alert-link" href={process.env.REACT_APP_MY_RESUME_LINK} target="_blank" rel="noreferrer">My RESUME</a>
+                    <p>Currently, I am Open and searching for a React Developer job and welcome any other opportunities like Software Engineer. Check out at&nbsp;<a className="alert-link" href={process.env.REACT_APP_MY_RESUME_LINK} target="_blank" rel="noreferrer">My RESUME</a>
                         !
                     </p>
                 </div>
